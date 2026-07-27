dbad7480fea7c76cf895b189344cd1a091e3281b
Bruno Becoski <brunobecoski@gmail.com>
2026-07-27 16:31:50 -0300

    configurando o Axe Core React

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
+import { axeAccessibilityReport } from "@/utils/axeAccessibilityReporter";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReport();
+  }, []);
+
   return <Component {...pageProps} />;
 }
